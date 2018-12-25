bdcaacbc2ec893fd9f837c42736984c1f501a395
Lenard Bernardo <lt_bernardo@hotmail.com>
2018-12-25 13:22:23 +1100

    typing environnent for safety

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,10 @@
+import { Environment } from "./environment.interface";
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
+export const environment: Environment = {
   production: false
 };
 
@@ -13,4 +15,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+import 'zone.js/dist/zone-error';  // Included with Angular CLI.
