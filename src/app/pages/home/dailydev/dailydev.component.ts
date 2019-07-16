998d86a1bca4cfb5b6d1c8c9f5c96bd039a243eb
Kevin Tenggara <kevin.tenggara@gmail.com>
2019-07-16 21:33:49 +1000

    Created Daily Dev Component (Page)
    Daily Devotions Page is created

---
+++ src/app/pages/home/dailydev/dailydev.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dailydev',
+  templateUrl: './dailydev.component.html',
+  styleUrls: ['./dailydev.component.scss'],
+})
+export class DailydevComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
