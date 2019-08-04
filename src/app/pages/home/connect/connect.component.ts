15eedff97c2ccfb7de903eda7364cd8ac19b1ece
Nick <n.wiranata@gmail.com>
2019-08-05 09:28:38 +1000

    connect page - fixed height, added arrows

--- src/app/pages/home/connect/connect.component.ts
+++ src/app/pages/home/connect/connect.component.ts
@@ -16,9 +16,35 @@ export class ConnectComponent implements OnInit
   ngOnInit() {
 
     this.connectGroups = [
-        { name: 'Young Mens', description: 'bunch of men test description' },
-        { name: 'Young Couples', description: 'Voluptate qui elit eu consequat ex anim non laboris cillum nostrud. Proident pariatur ipsum incididunt magna ut ad magna fugiat fugiat. Cillum dolor eu occaecat commodo nostrud.' }
-    ]
-  }
+        {
+          name: 'Young Mens',
+          description: 'bunch of men test description',
+        },
 
+        {
+          name: 'Young Womens',
+          description: 'Voluptate qui elit eu consequat',
+        },
+
+        {
+          name: 'Young Couples',
+          description: 'Voluptate qui elit eu consequat',
+        },
+
+        {
+          name: 'Young Families',
+          description: 'Voluptate qui elit eu consequat',
+        },
+
+        {
+          name: 'Board Games',
+          description: 'Voluptate qui elit eu consequat',
+        },
+
+        {
+          name: 'Sports',
+          description: 'Voluptate qui elit eu consequat',
+        },
+    ];
+  }
 }
